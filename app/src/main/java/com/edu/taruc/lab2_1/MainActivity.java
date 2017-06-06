d02ad072ecb62d7cef59ad96362aa7d0608abaeb
Li Shao Ming <dynalsm@gmail.com>
2017-06-06 10:18:47 +0800

    initial

---
+++ app/src/main/java/com/edu/taruc/lab2_1/MainActivity.java
@@ -0,0 +1,27 @@
+package com.edu.taruc.lab2_1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+
+public class MainActivity extends AppCompatActivity {
+    private EditText editTextMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editTextMessage = (EditText)findViewById(R.id.editTextMessage);
+    }
+    public void sendMessage(View view){
+        String message = editTextMessage.getText().toString();
+        Intent intent = new Intent(this, SecondActivity.class);
+        //insert parameter to an intent
+        intent.putExtra("MESSAGE", message);
+
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/edu/taruc/lab2_1/SecondActivity.java
@@ -0,0 +1,27 @@
+package com.edu.taruc.lab2_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+
+import org.w3c.dom.Text;
+
+public class SecondActivity extends AppCompatActivity {
+private TextView textViewMessage;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        textViewMessage = (TextView)findViewById(R.id.textViewMessage);
+
+        Bundle extra = getIntent().getExtras();
+
+        if(! extra.isEmpty()){
+            String message = extra.getString("MESSAGE");
+            textViewMessage.setText(message);
+        }
+
+    }
+}
